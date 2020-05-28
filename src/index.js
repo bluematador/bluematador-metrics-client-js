d95e8ff813bd750c483be1e419087cd9df5ea061
Hunter <hunter@bluematador.com>
2020-05-28 14:39:39 -0600

    housed client methods inside init function

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
 const StatsD = require('hot-shots');
 const { sanitize } = require('./sanitizer')
 
-let client = null
-
-const gauge = async (gauge, value, tags, errorHandler, successHandler) => {
-  try {
-    if(sanitize(gauge, value, null, tags)) {
-      await client.gauge(gauge, value, 1, tags, (err, bytes) => {
-        if(err) {
-          typeof errorHandler === 'function' && errorHandler(err)
-        } else {
-          typeof successHandler === 'function' && successHandler(bytes)
-        }
-      });
-    }
-  } catch(err) {
-    typeof errorHandler === 'function' && errorHandler(err)
-  }
-};
-
-const counter = async (counter, value, tags, errorHandler, successHandler) => {
-  try {
-    if(sanitize(counter, value, null, tags)) {
-      await client.increment(counter, 1, 1, tags, (err, bytes) => {
-        if(err) {
-          typeof errorHandler === 'function' && errorHandler(err)
-        } else {
-          typeof successHandler === 'function' && successHandler(bytes)
-        }
-      });
-    }
-  } catch(err) {
-    typeof errorHandler === 'function' && errorHandler(err)
-  }
-};
-
-const close = () => {
-  client.close()
-}
-
 const init = (host, port, errorHandler) => {
+  let client = null
+
+  const gauge = async (gauge, value, sampleRate, tags, responseHandler) => {
+    // if(sampleRate && typeof sampleRate !== 'number') {
+
+    //   tags = sampleRate
+    //   typeof tags === 'function' ? responseHandler = tags : responseHandler = null
+    // }
+    // console.log(sampleRate, tags, responseHandler)
+    try {
+      if(sanitize(gauge, value, sampleRate, tags)) {
+        await client.gauge(gauge, value, 1, tags, (err, bytes) => {
+          if(err) {
+            typeof responseHandler === 'function' && responseHandler(err)
+          } else {
+            typeof responseHandler === 'function' && responseHandler(null, bytes)
+          }
+        });
+      }
+    } catch(err) {
+      typeof responseHandler === 'function' && responseHandler(err)
+    }
+  };
+
+  const counter = async (counter, value, tags, sampleRate, errorHandler, successHandler) => {
+    try {
+      if(sanitize(counter, value, null, tags)) {
+        await client.increment(counter, 1, 1, tags, (err, bytes) => {
+          if(err) {
+            typeof errorHandler === 'function' && errorHandler(err)
+          } else {
+            typeof successHandler === 'function' && successHandler(bytes)
+          }
+        });
+      }
+    } catch(err) {
+      typeof errorHandler === 'function' && errorHandler(err)
+    }
+  };
+
+  const close = () => {
+    client.close()
+  }
+
+
   try {
     if(typeof host === 'number') {
       throw new Error('The host argument must be one of type string or falsy. Recieved type number');
--- src/mockClient.js
+++ src/mockClient.js
@@ -2,16 +2,15 @@ const blueMatador = require("./index")
 
 let client = blueMatador.init()
 
-client.gauge('cpu_util', 32.25, ["env:prod"], err => {
+client.gauge('cpu_util', 32.25, 1, ["env:prod"], (err, resp) => {
   console.log(err)
-}, success => {
-  console.log(success)
+  console.log(resp)
   client.close()
 })
 
-client.counter('homepage_clicks', ["env:dev"], err => {
-  console.log(err)
-}, success => {
-  console.log(success)
-  client.close()
-})
+// client.counter('homepage_clicks', ["env:dev"], err => {
+//   console.log(err)
+// }, success => {
+//   console.log(success)
+//   client.close()
+// })
