d922d317ad6b406cccc94f534ce4825a41aa5f49
Hunter <hunter@bluematador.com>
2020-07-08 10:06:18 -0600

    updated testing to send options object

--- src/options.js
+++ src/options.js
@@ -34,59 +34,36 @@ const createGaugeMetric = (prefix, name, value, sampleRate, labels) => {
   if (!value || typeof value !== 'number') {
     throw new Error('Metric value is missing or is an invalid type. Must be type number');
   }
-  const metric = {
+  return {
     name: sanitizeName(name, prefix),
     value,
     sampleRate,
-    labels
-  };
-  if (typeof sampleRate !== 'number') {
-    metric.labels = typeof sampleRate === 'object' ? sampleRate : [];
-    metric.sampleRate = 1;
-  } else if (!labels) {
-    metric.labels = [];
+    labels: formatLabels(labels)
   }
-  metric.labels = formatLabels(metric.labels);
-
-  return metric;
 };
 
 const createCountMetric = (prefix, name, value, sampleRate, labels) => {
   if (!name || typeof name !== 'string') {
     throw new Error('Metric name is missing or is an invalid type. Must be type string');
   }
-  const metric = {
+  return {
     name: sanitizeName(name, prefix),
     value,
     sampleRate,
-    labels
+    labels: formatLabels(labels)
   };
-  if (typeof value === 'number' && typeof sampleRate !== 'number') {
-    metric.sampleRate = 1;
-    metric.labels = sampleRate;
-  } else if (typeof value === 'object') {
-    metric.sampleRate = 1;
-    metric.value = 1;
-    metric.labels = value;
-  } else {
-    metric.sampleRate = 1;
-    metric.value = 1;
-    metric.labels = [];
-  }
-  metric.labels = formatLabels(metric.labels);
-
-  return metric;
 };
 
 const init = (options) => {
+  const initOptions = options || {}
   // Prefer passed in host, then env variable, then default to localhost
-  const clientHost = options.host || process.env.BLUEMATADOR_AGENT_HOST || 'localhost';
+  const clientHost = initOptions.host || process.env.BLUEMATADOR_AGENT_HOST || 'localhost';
   if (typeof clientHost !== 'string') {
     throw new Error('Host must be a string');
   }
 
   // Prefer passed in port, then env varible if it is a number, then default to 8767
-  let clientPort = options.port;
+  let clientPort = initOptions.port;
   if (process.env.BLUEMATADOR_AGENT_PORT) {
     const parsedPort = parseInt(process.env.BLUEMATADOR_AGENT_PORT, 10);
     if (isNaN(parsedPort)) {
@@ -101,9 +78,9 @@ const init = (options) => {
   }
 
   // Sanitize the prefix
-  let metricPrefix = options.prefix;
-  if (options.prefix && typeof options.prefix === 'string') {
-    metricPrefix = options.prefix.replace(/:|\|/gi, '_');
+  let metricPrefix = initOptions.prefix;
+  if (initOptions.prefix && typeof initOptions.prefix === 'string') {
+    metricPrefix = initOptions.prefix.replace(/:|\|/gi, '_');
   }
 
   const client = new StatsD({
@@ -112,10 +89,11 @@ const init = (options) => {
     tagSeparator: '#',
   });
 
-  const gauge = (name, value, options, responseHandler) => {
+  const gauge = (name, value, options) => {
+    const metricOptions = options || {}
     return new Promise((resolve, reject) => {
       try {
-        const metric = createGaugeMetric(metricPrefix, name, value, options.sampleRate || 1, options.labels || "");
+        const metric = createGaugeMetric(metricPrefix, name, value, metricOptions.sampleRate || 1, metricOptions.labels || "");
         if (sanitize(metric)) {
           client.gauge(metric.name, metric.value, metric.sampleRate, metric.labels, (err) => {
             if (err) {
@@ -131,10 +109,12 @@ const init = (options) => {
     });
   };
 
-  const count = (name, options, responseHandler) => {
+  const count = (name, options) => {
+    const metricOptions = options || {}
+    console.log(metricOptions)
     return new Promise((resolve, reject) => {
       try {
-        const metric = createCountMetric(metricPrefix, name, options.value || 1, options.sampleRate || 1, options.labels || "", responseHandler);
+        const metric = createCountMetric(metricPrefix, name, metricOptions.value || 1, metricOptions.sampleRate || 1, metricOptions.labels || "");
         if (sanitize(metric)) {
           client.increment(metric.name, metric.value, metric.sampleRate, metric.labels, (err) => {
             if (err) {
